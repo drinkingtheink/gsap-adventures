6c5f13100dabaad27f7fcf3cd0968042cc807b85
Jason Harrison <jharrison@jharrison-mbp.austin.q2technology.com>
2019-12-02 13:15:35 -0600

    Scaffolding of a stage and the first animation.

---
+++ src/components/Animation1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="animation-1">
+    <h2>Animation 1</h2>
+
+    <div ref="box" class="box"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Animation1'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.animation-1 {
+	.box { 
+		height: 60px; 
+		width: 60px; 
+		background: red; 
+	}
+}
+</style>
